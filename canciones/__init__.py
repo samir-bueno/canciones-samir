b1cadd2ccd06278e42848ba50b36f1b064629a72
samir-bueno <samirbueno585@gmail.com>
2024-04-29 12:07:39 -0300

    ejercicio 3 en proceso

--- canciones/__init__.py
+++ canciones/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
+
+with app.app_context():
+  from db import init_app
+  init_app(app)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
---
+++ canciones/db.py
@@ -0,0 +1,51 @@
+import sqlite3
+import os
+import click
+from flask import current_app, g, app
+
+db_folder = current_app.instance_path
+db_name = "informacion.sqlite"
+db_file = os.path.join(db_folder, db_name)
+db_sql_file = "datos.sql"
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            db_file,
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+      # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    db = get_db()
+
+    with current_app.open_resource('datos.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+ 
