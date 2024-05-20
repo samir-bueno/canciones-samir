ead3fa15a2220081f8b229b1ae965542f17c288c
samir-bueno <samirbueno585@gmail.com>
2024-05-20 09:32:14 -0300

    paso 4 hecho pero sin css, luego lo hare

--- canciones/__init__.py
+++ canciones/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
 
+#en blueprint
+#from flask import Flask, render_template, Blueprint
+#bp = Blueprint("actor",__name__, url_prefix="/actor")
+#def detalle(id):
+  # def actores()
+    #  .....
 
 app = Flask(__name__)
 
@@ -16,19 +22,28 @@ def hello():
 @app.route('/canciones')
 def canciones():
     consulta1 = """
-           select name from tracks
-        """
+            select name from tracks
+          """
     consulta2 = """
+             select name from genres
+          """
+    consulta3 = """
              select name from artists
           """
     base_de_datos = db.get_db()
+
     resultado = base_de_datos.execute(consulta1)
-    lista_de_resultados = resultado.fetchall()
+    lista_de_canciones = resultado.fetchall()
 
     resultado = base_de_datos.execute(consulta2)
+    lista_de_generos = resultado.fetchall()
+
+    resultado = base_de_datos.execute(consulta3)
     lista_de_bandas = resultado.fetchall()
 
 
-    pagina = render_template("canciones.html", canciones=lista_de_resultados, bandas=lista_de_bandas)
+
+
+    pagina = render_template("canciones.html", canciones=lista_de_canciones, generos=lista_de_generos, bandas=lista_de_bandas)
     return pagina
 
