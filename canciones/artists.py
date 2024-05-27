40c122de9e5f2413f45af8ac9314ed96fb78505c
samir-bueno <samirbueno585@gmail.com>
2024-05-27 12:01:27 -0300

    estoy en el paso 6 intentando que las canciones sean links y en cada link se envie al artista o banda

--- canciones/artists.py
+++ canciones/artists.py
@@ -19,6 +19,27 @@ def bandas():
     pagina = render_template("bandas.html", bandas=lista_de_bandas)
     return pagina
 
+@bp.route("/<int:id>")
+def detalle(id):
+    consulta1 = """
+        SELECT Name, ArtistId FROM artists
+        WHERE ArtistId = ? ;
+    """
+    consulta2 = """
+        SELECT t.name, g.Title FROM tracks t
+        JOIN albums g ON t.AlbumId = g.AlbumId
+        JOIN artists a ON g.ArtistId = a.ArtistId
+        WHERE a.ArtistId = ? ;
+    """
+    base_de_datos = db.get_db()
+    resultado = base_de_datos.execute(consulta1, (id, ))
+    artista = resultado.fetchone()
+
+    resultado = base_de_datos.execute(consulta2, (id, ))
+    lista_canciones = resultado.fetchall()
+
+    pagina = render_template("detalle_artista.html", artista = artista, canciones = lista_canciones)
+    return pagina
 
 
 
